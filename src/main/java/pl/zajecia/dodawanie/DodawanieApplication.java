32a6ef2c1e325fb77f26557a0a935b669e410e9c
ZygarM251 <maeusz251@gmail.com>
2026-05-07 16:39:23 +0200

    add MockMvc test

--- src/main/java/pl/zajecia/dodawanie/DodawanieApplication.java
+++ src/main/java/pl/zajecia/dodawanie/DodawanieApplication.java
@@ -1,11 +1,24 @@
 package pl.zajecia.dodawanie;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
 public class DodawanieApplication {
 
     public static void main(String[] args) {
+        SpringApplication.run(DodawanieApplication.class, args);
     }
 
     public int add(int a, int b) {
         return a * b;
+    }
+
+    @GetMapping("/")
+    public String get() {
+        return "Hello Jenkins";
     }
 }
--- src/test/java/pl/zajecia/dodawanie/DodawanieApplicationTest.java
+++ src/test/java/pl/zajecia/dodawanie/DodawanieApplicationTest.java
@@ -1,13 +1,34 @@
 package pl.zajecia.dodawanie;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class DodawanieApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     public void add() {
         DodawanieApplication app = new DodawanieApplication();
-        assertEquals(10, app.add(5, 5));
+        org.junit.jupiter.api.Assertions.assertEquals(10, app.add(5, 5));
+    }
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello Jenkins"));
     }
 }
